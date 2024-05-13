e2dee6b26eb191642c5ced3fe9f4401573b97c3b
D5 <dimastd333@gmail.com>
2024-05-13 23:42:13 +0500

    кирка

---
+++ Assets/_Domain/Items/Pickaxe.cs
@@ -0,0 +1,22 @@
+﻿public class Pickaxe : Entity<Pickaxe, PickaxeScript>
+{
+    public Pickaxe(PickaxeScript gameObject) : base(gameObject)
+    {
+    }
+
+    public override string Title => "Кирка";
+
+    public override bool IsItem => true;
+
+    public override bool CanCleaned => false;
+
+    public override bool CanUse(IEntity target)
+    {
+        return true;
+    }
+
+    public override void Use(IEntity target)
+    {
+        base.Use(target);
+    }
+}
